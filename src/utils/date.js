197a8699038982a5fd1beedb8c24114ee5d5061b
Wanlin-Lu <luwanlin@126.com>
2020-02-14 21:17:48 +0800

    url and request

---
+++ src/utils/date.js
@@ -0,0 +1,13 @@
+export function getFormatDate(utc) {
+	const date = new Date(utc)
+	const year = date.getFullYear()
+	const month = date.getMouth()
+	const day = date.getDate()
+	const hour = date.getHours()
+	const minute = date.getMinutes()
+	const second = date.getSeconds()
+	function pd(unit) {
+		return unit > 9 unit : '0'+unit 
+	}
+	return `${year}-${pd(month)}-${pd(day)} ${pd(hour)}:${pd(minute)}`
+}
---
+++ src/utils/url.js
@@ -0,0 +1,35 @@
+const postListFilter = {
+	fields: ['id', 'title', 'author', 'vote', 'updatedAt'],
+	limit: 10,
+	order: 'updatedAt DESC',
+	include: 'authorPointer',
+	includefilter: { user: { fields: ['id', 'username'] } }
+};
+const postByIdFilter = id => ({
+	fields: ['id', 'title', 'author', 'vote', 'updatedAt', 'content'],
+	where: { id: id },
+	include: 'authorPointer',
+	includefilter: { user: { fields: ['id', 'username'] } }
+});
+const commentListFilter = postId => ({
+	fields: ['id', 'author', 'updatedAt', 'content'],
+	where: { post: postId },
+	limit: 20,
+	order: 'updatedAt DESC',
+	include: 'authorPointer',
+	includefilter: { user: { fields: ['id', 'username'] } }
+});
+
+function encodeFilter(filter) {
+	return encodeURIComponent(JSON.stringify(filter));
+}
+
+export default {
+	login: () => `/user/login`,
+	getPostList: () => `/post?filter=${encodeURIComponent(postListFilter)}`,
+	getPostById: id => `/post?filter=${encodeURIComponent(postByIdFilter(id))}`,
+	createPost: () => `/post`,
+	updatePost: id => `/post/${id}`,
+	getCommentList: postId => `/comment?filter=${encodeURIComponent(commentListFilter(postId))}`,
+	createComment: () => `/comment`
+}
