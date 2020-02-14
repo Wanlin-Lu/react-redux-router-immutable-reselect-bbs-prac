dc04e00470bf5d1ed540bd0403541b6055566b19
Wanlin-Lu <luwanlin@126.com>
2020-02-14 22:36:58 +0800

    redux-app

---
+++ src/redux/modules/app.js
@@ -0,0 +1,52 @@
+const initialState = {
+	requestQuantity: 0,
+	error: null
+}
+
+export const types = {
+	START_REQUEST: "APP/START_REQUEST",
+	FINISH_REQUEST: "APP/FINISH_REQUEST",
+	SET_ERROR: "APP/SET_ERROR",
+	REMOVE_ERROR: "APP/REMOVE_ERROR"
+}
+
+export const actions = {
+	startRequest: () => ({
+		type: types.START_REQUEST
+	}),
+	finishRequest: () => ({
+		type: types.FINISH_REQUEST
+	}),
+	setError: error => ({
+		type: types.SET_ERROR,
+		error 
+	}),
+	removeError: () => ({
+		type: types.REMOVE_ERROR
+	})
+}
+
+const reducers = (state = initialState, action) => {
+	switch(action.type) {
+		case types.START_REQEST:
+			return { ...state, requestQuantity: state.requestQuantity + 1 }
+		case types.FINISH_REQEST:
+			return { ...state, requestQuantity: state.requestQuantity - 1 }
+		case types.SET_ERROR:
+			return { ...state, error: action.error }
+		case types.REMOVE_ERROR:
+			return { ...state, error: null }
+		default:
+			return state 
+	}
+}
+
+export default reducers 
+
+export const getRequestQuantity = state => {
+	return state.app.requestQuantity
+}
+
+export const getError = state => {
+	return state.app.error 
+}
