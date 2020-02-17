4c7f2d66344b3c8bc75f168821c42d2668aa6743
Wanlin-Lu <luwanlin@126.com>
2020-02-17 19:50:04 +0800

    redux-users

---
+++ src/redux/modules/users.js
@@ -0,0 +1,22 @@
+import { types as postTypes } from './posts'
+import { types as commentTypes } from './comments'
+
+const initialState = {}
+
+const reducer = (state = initialState, action) => {
+	switch(action.type) {
+		case postTypes.FETCH_ALL_POST:
+		case commentTypes.FETCH_COMMENTS:
+			return { ...state, ...action.users }
+		case authTypes.FETCH_POST:
+			return { ...state, [action.userId]: action.user }
+		default:
+			return state
+	}
+}
+
+export default reducer
+
+export const getUsers = state => state.users 
+
+export const getUserById = (state, Id) => state.users[Id]
